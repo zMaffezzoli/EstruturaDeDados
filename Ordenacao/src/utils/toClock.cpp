43ecf6f13c616b10fb09adc189c1374cab47f27f
zMaffezzoli <brunomaffezzolirodrigues0@gmail.com>
2026-02-28 01:10:45 -0300

    Saida mais completa para analise

--- Ordenacao/src/utils/toClock.cpp
+++ Ordenacao/src/utils/toClock.cpp
@@ -15,7 +15,9 @@ void medirTempo(function<void (vector<int>&)> metodoOrdenamento, vector<int> &va
     metodoOrdenamento(valores); // executa a função de ordenação
     auto fim = high_resolution_clock::now();
 
-    auto duracao = duration_cast<nanoseconds>(fim - inicio).count();
+    auto duracaoNs = duration_cast<nanoseconds>(fim - inicio).count();
+    auto duracaoMs = duration_cast<milliseconds>(fim - inicio).count();
+    // ≈ em UTF-8 = 0xE2 0x89 0x88
 
-    cout << "Tempo de execucao " << metodo << " com " << valores.size() << " valores: " << duracao << " ns" << endl;
+    cout << "Tempo de execucao " << metodo << " com " << valores.size() << " valores: " << duracaoNs << "ns" " \xE2\x89\x88 " << duracaoMs << "ms" << endl;
 }
